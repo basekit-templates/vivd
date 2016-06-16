c0c7fd17e01b230f2cb89bc873973426a823ce57
vincenttran <vincent.tran@basekit.com>
2016-06-16 11:59:13 +0100

    new template commit

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+// Makes the whole ecom product tiles clickable
+$(document).ready(function() {
+
+    $('.product-item').each(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            $(this).addClass("clickable");
+        }
+    });
+
+
+    metaKeyPressed = false;
+
+    $(window).keydown(function(e) {
+        if (e.ctrlKey || e.metaKey) {
+            metaKeyPressed = true;
+        }
+    });
+
+
+    $('.product-item').click(function() {
+
+        var href = $(this).find("a").attr("href");
+        if(href) {
+
+            if (metaKeyPressed == true) {
+                 window.open(href, '_blank');
+            } else {
+                window.location = href;
+            }
+        }
+
+    });
+
+});
